1e15be25562464fe546e1a595426ac4bed0008c2
Игорь Чудакин <Chudakin98@gmail.com>
2021-10-15 09:31:35 +0700

    подготовка скрипта точки реплик

--- Assets/Scripts/Environment/ReplicPointScript.cs
+++ Assets/Scripts/Environment/ReplicPointScript.cs
@@ -1,27 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
 public class ReplicPointScript : MonoBehaviour
 {
     [HideInInspector]public ReplicDispether replicDispether;
     public List<ReplicItem> replicas;
-    public List<TranslateScript> movingCubes;
-
-    private void Awake()
-    {
-        GetComponent<BoxCollider>().enabled = false;
-    }
-
-    private void Start()
-    {
-        Invoke("ToStart", 1);
-    }
 
     public void PlayReplicas()
     {
-        GetComponent<BoxCollider>().enabled = false;
         replicDispether.AddInList(replicas);
 
         float delayTime = 0;
@@ -31,19 +17,6 @@ public class ReplicPointScript : MonoBehaviour
             delayTime += item.clip.length;
         }
         delayTime += 2;
-        Invoke("UseCubes", delayTime);
         Destroy(gameObject, delayTime + 1);
-    }
-
-    private void UseCubes()
-    {
-        foreach (var item in movingCubes)
-        {
-            item.ChangePosition();
-        }
-    }
-    private void ToStart()
-    {
-        GetComponent<BoxCollider>().enabled = true;
     }
 }
