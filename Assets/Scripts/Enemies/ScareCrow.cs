6483a3198409162b5589b1ccc2944eefec002fd8
menem8888 <menem8888@gmail.com>
2021-10-24 19:54:23 +0700

    Сделан скрипт ScareCrow

--- Assets/Scripts/Enemies/ScareCrow.cs
+++ Assets/Scripts/Enemies/ScareCrow.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class ScareCrow : HellEnemy
 {
     [SerializeField, Tooltip("Дальность прыжка"), Range(1, 100)] protected float jumpDistance = 15;
-    [SerializeField, Tooltip("Задержка перед телепортацией"), Range(1, 100)] protected float jumpDelay = 5;
 
 
 
     protected override void Update()
     {
         
-        if ((target.transform.position - transform.position).magnitude < jumpDistance+1 && (target.transform.position - transform.position).magnitude > jumpDistance+1)
+        if ((target.transform.position - transform.position).magnitude < jumpDistance+1 && (target.transform.position - transform.position).magnitude > jumpDistance-1)
         {
-            //anim.SetBool("Jump",true);
+            anim.SetBool("Jump",true);
         }
         base.Update();
         
     }
     
-    public void SpecialMove()
+    public override  IEnumerator SpecialMove()
     {
-        transform.position = target.transform.position + Vector3.back;
+        transform.position = target.transform.position + Vector3.back*1;
+        transform.rotation = target.transform.rotation;
+        anim.SetBool("Jump",false);
+        yield return new WaitForSeconds(0);
 
     }
 }
